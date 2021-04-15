7de0f6572a516da92b1d1e423b0fa0508ae17163
ialonsordaw <ialonsordaw1@gmail.com>
2021-04-15 18:23:10 +0200

    Mejoras

--- src/ut7/agenda/modelo/ComparatorCon.java
+++ src/ut7/agenda/modelo/ComparatorCon.java
@@ -9,13 +9,13 @@ public class ComparatorCon implements Comparator<Contacto>{
 	 * @return orden
 	 */
 	@Override
-	public int compare(Contacto o1, Contacto o2) {
-		if (o1.getApellidos().compareTo(o2.getApellidos()) == 0) {
-			if (o1.getNombre().compareTo(o2.getNombre()) < 0) return -1;
-			else if (o1.getNombre().compareTo(o2.getNombre()) > 0) return 1;
+	public int compare(Contacto c1, Contacto c2) {
+		if (c1.getApellidos().compareTo(c2.getApellidos()) == 0) {
+			if (c1.getNombre().compareTo(c2.getNombre()) < 0) return -1;
+			else if (c1.getNombre().compareTo(c2.getNombre()) > 0) return 1;
 			else return 0;
 		}
-		else if (o1.getApellidos().compareTo(o2.getApellidos()) < 0) return -1;
+		else if (c1.getApellidos().compareTo(c2.getApellidos()) < 0) return -1;
 		else return 1;
 	}
 }
